fc05c574ef9a751a5212366fe8ba37aa3b6339c3
Sanket Panda <pandasa123@gmail.com>
2019-08-19 16:25:29 -0400

    Finally got this working

--- UploadToBlobStorage/__init__.py
+++ UploadToBlobStorage/__init__.py
@@ -16,17 +16,6 @@ def list_all_blobs(blobService: BlockBlobService, container_name: str) -> []:
     return names
 
 
-def upload_blob(blobService: BlockBlobService, container_name: str, blob_name: str, blob: bytes) -> str:
-    """Put Blob from base64 into Azure Blob Storage Container and generate URL"""
-    blobService.create_blob_from_bytes(
-        container_name=container_name, blob_name=blob_name, blob=blob)
-    logging.info('Uploaded Image')
-    blob_source_url = blobService.make_blob_url(
-        container_name=container_name, blob_name=blob_name)
-    logging.info('Generated Blob URL')
-    return blob_source_url
-
-
 # Not needed in final
 def img_filesystem_to_bytes(img: str) -> bytes:
     """Load image from filesystem and encode to base64"""
@@ -39,7 +28,35 @@ def img_filesystem_to_bytes(img: str) -> bytes:
 
 def base64_to_byte_array(img_base64: str) -> bytes:
     """Base64 string to img bytes"""
-    return str.encode(img_base64)
+    if img_base64[0] == 'b' and img_base64[1] == "'":
+        return eval(img_base64)
+
+    img_base64 = "b'"+img_base64+"'"
+    return eval(img_base64)
+
+
+def upload_file_from_bytes(img: bytes, blobService: BlockBlobService, container_name: str, blob_name: str) -> str:
+    import os
+    import tempfile
+
+    fd, path = tempfile.mkstemp()
+    with os.fdopen(fd, 'wb') as tmp:
+        tmp.write(base64.b64decode(img))
+
+    url = upload_file(blobService=blobService,
+                      container_name=container_name, blob_name=blob_name, path=path)
+    os.remove(path)
+    return url
+
+
+def upload_file(blobService: BlockBlobService, container_name: str, blob_name: str, path: str) -> str:
+    blobService.create_blob_from_path(
+        container_name=container_name, blob_name=blob_name, file_path=path)
+
+    logging.info('Uploaded Image')
+    blob_source_url = blobService.make_blob_url(
+        container_name=container_name, blob_name=blob_name)
+    return blob_source_url
 
 
 def upload_to_blob_storage(account_name: str, account_key: str, blob_name: str, container_name: str, img_base64: str) -> str:
@@ -47,12 +64,10 @@ def upload_to_blob_storage(account_name: str, account_key: str, blob_name: str, 
     block_blob_service = BlockBlobService(
         account_name=account_name, account_key=account_key)
     logging.info('Initalised Blob Service')
-    img_bytes: bytes = base64_to_byte_array(img_base64=img_base64)
 
-    logging.info('Captured Image')
-    url = upload_blob(blobService=block_blob_service,
-                      container_name=container_name,
-                      blob_name=blob_name, blob=img_bytes)
+    img_bytes: bytes = base64_to_byte_array(img_base64=img_base64)
+    url = upload_file_from_bytes(img=img_bytes, blobService=block_blob_service,
+                                 container_name=container_name, blob_name=blob_name)
     return url
 
 
