f994f1f1c28b8177862661c6dfd1e76b0510d94b
Sanket Panda <pandasa123@gmail.com>
2019-08-20 21:20:29 -0400

    Added eslint + AD B2C

--- expensely-frontend/src/index.js
+++ expensely-frontend/src/index.js
@@ -1,12 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import b2cauth from 'react-azure-adb2c'
+import './index.css'
+import App from './App'
+import * as serviceWorker from './serviceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+b2cauth.initialize({
+	instance: 'https://login.microsoftonline.com/tfp/',
+	tenant: 'expensely.onmicrosoft.com',
+	signInPolicy: 'B2C_1_expensely_signup',
+	applicationId: 'ac6588f2-aba7-4638-b015-89ac51cd01d0',
+	cacheLocation: 'sessionStorage',
+	scopes: ['https://expensely.onmicrosoft.com/api/user_impersonation'],
+	redirectUri: 'http://localhost:3000',
+	postLogoutRedirectUri: window.location.origin
+})
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+b2cauth.run(() => {
+	ReactDOM.render(<App />, document.getElementById('root'))
+	serviceWorker.unregister()
+})
