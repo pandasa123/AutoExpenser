3770d217ca0d9abc85f084de7064715beb9dd057
Sanket Panda <pandasa123@gmail.com>
2019-08-22 10:08:10 -0400

    Footer done

--- expensely-frontend/src/Layout/Footer.jsx
+++ expensely-frontend/src/Layout/Footer.jsx
@@ -1,13 +1,41 @@
 import React from 'react'
+import { Text } from 'office-ui-fabric-react'
 
 const FooterStyle = {
     backgroundColor: '#252423',
-    height: '100px'
+    height: '32px',
+    padding: '32px',
+    display: 'flex',
+    alignItems: 'center'
+}
+
+const TextStyleSubdued = {
+    root: {
+        color: 'white'
+    }
+}
+
+const TextStyleEmphasis = {
+    root: {
+        color: 'white',
+        fontWeight: 800
+    }
 }
 
 const Footer = () => {
     return (
-        <div style={FooterStyle}/>
+        <div style={FooterStyle}>
+            <span>
+                <Text variant="mediumPlus" styles={TextStyleSubdued}>Created by </Text>
+                <a href={'https://sanketpanda.com'} style={{
+                    color: 'inherit',
+                    textDecoration: 'inherit'
+                }}
+                >
+                    <Text variant="mediumPlus" styles={TextStyleEmphasis}>Sanket Panda</Text>
+                </a>
+            </span>
+        </div >
     )
 }
 
