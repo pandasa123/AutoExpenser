32c4793da6ba0d9b8a148cc253ab306c44225f7d
Sanket Panda <pandasa123@gmail.com>
2019-08-23 04:39:30 -0400

    Started form building. Need to add dates + multi file upload

--- expensely-frontend/src/pages/NewReport.jsx
+++ expensely-frontend/src/pages/NewReport.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import { Text, TextField, Calendar } from 'office-ui-fabric-react'
 
 const NewReport = () => {
     return (
-        <div>
-            FEWOFEWFOIWJF
+        <div style={{
+            display: 'flex',
+            flexDirection: 'column',
+            padding: '32px',
+            maxWidth: '800px'
+        }}>
+            <Text variant="xLarge">Create a New Report</Text>
+            <br />
+            <TextField label="Trip Name" defaultValue="New Report Name" />
+            
         </div>
     )
 }
