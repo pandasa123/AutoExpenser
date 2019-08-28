d13b0205f9b183bed77b7529e3db7d249acfb3ff
Sanket Panda <pandasa123@gmail.com>
2019-08-27 20:49:47 -0400

    Added PolicyReview Trigger

---
+++ PolicyReview/__init__.py
@@ -0,0 +1,96 @@
+import logging
+from azure.cosmosdb.table.tableservice import TableService
+import azure.functions as func
+
+
+def get_policy_details(company_name: str) -> dict:
+    account_name: str = 'expensely-db'
+    account_key: str = 'nSXLSU2AhgZqHCPxmdouuP5uaDApnVuyPihIpoZkf8CbhHFIZNnakSKXObVhumv1ogQJPplSMKFVwvvI0S9adA=='
+    protocol: str = 'https'
+    table_endpoint: str = 'https://expensely-db.table.cosmos.azure.com:443/'
+
+    table_service = TableService(
+        account_name=account_name, account_key=account_key)
+
+    connection_string = "DefaultEndpointsProtocol={};AccountName={};AccountKey={};TableEndpoint={};".format(
+        protocol, account_name, account_key, table_endpoint)
+
+    table_service = TableService(endpoint_suffix="table.cosmos.azure.com",
+                                 connection_string=connection_string)
+
+    query_string: str = "PartitionKey eq '{company_name}'".format(
+        company_name=company_name)
+    policies = table_service.query_entities(
+        table_name='company-policies', filter=query_string)
+
+    for policy in policies:
+        return policy
+
+    return {}
+
+
+def parse_policy(policy: dict) -> dict:
+    policy_string: str = policy['RowKey']
+    policy_arr: [] = policy_string.split(sep=';')
+    policies = {}
+    for policy in policy_arr:
+        policy_details = policy.split(sep='=')
+        if len(policy_details) == 2:
+            policies[policy_details[0]] = policy_details[1]
+
+    return policies
+
+
+def review_items(items: [], total: float, policy: dict) -> dict:
+    approved_total: float = 0.0
+    num_approved: int = 0
+    if 'max_item_price' in policy:
+        for item in items:
+            for key, value in item.items():
+                if float(policy['max_item_price']) >= value:
+                    num_approved += 1
+                    approved_total += value
+    if 'total_meal_price' in policy:
+        if approved_total > float(policy['total_meal_price']):
+            approved_total = float(policy['total_meal_price'])
+
+    return {
+        'num_approved': num_approved,
+        'approved_total': round(approved_total, 2)
+    }
+
+
+def main(documents: func.DocumentList, doc: func.Out[func.Document]) -> str:
+    if documents:
+        for document in documents:
+            if document['status'] == 'Not Reviewed':
+                logging.info('Document id: %s', document.id)
+                company_name: str = document['company_name']
+                items: [] = eval(document['items'])
+                total: float = float(document['total'])
+                policy_string: dict = get_policy_details(
+                    company_name=company_name)
+                if len(policy_string) != 0:
+                    parsed_policy_dict: dict = parse_policy(
+                        policy=policy_string)
+                    reviewed: dict = review_items(
+                        items=items, total=total, policy=parsed_policy_dict)
+                    del document['etag']
+                    del document['Timestamp']
+                    document['status'] == 'Reviewed'
+                    document['approved'] = reviewed['approved_total']
+                    document['num_approved'] = reviewed['num_approved']
+                    logging.info(reviewed)
+                    doc.set(document)
+
+
+if __name__ == "__main__":
+    company_name = 'Microsoft'
+    policy_string: dict = get_policy_details(company_name=company_name)
+    items = [{'1 CONGRESS BURGER': 16.0}, {
+        '1 CAPTAIN MORGAN SPICED': 10.49}, {'1 $ DOUBLE': 5.0}]
+    total = 34.09
+    if len(policy_string) != 0:
+        parsed_policy_dict: dict = parse_policy(policy=policy_string)
+        reviewed: dict = review_items(
+            items=items, total=total, policy=parsed_policy_dict)
