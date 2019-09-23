1890f786f122238d0362f9af2dbfb157f98f9303
Sanket Panda <pandasa123@gmail.com>
2019-09-23 14:09:17 -0400

    Updated AuthProvider

--- expensely-frontend/src/index.tsx
+++ expensely-frontend/src/index.tsx
@@ -2,25 +2,52 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import { initializeIcons } from 'office-ui-fabric-react';
+import { initializeIcons, Spinner, SpinnerSize } from 'office-ui-fabric-react';
 import './index.css';
-import { AzureAD } from 'react-aad-msal';
+import { AzureAD, AuthenticationState, IAccountInfo } from 'react-aad-msal';
 import { AuthProviderFactory } from './utils/AuthProviderFactory';
 
 initializeIcons();
 
+interface IAzureADFunctionProps {
+  login: () => void;
+  logout?: () => void;
+  authenticationState: AuthenticationState;
+  accountInfo: IAccountInfo;
+}
+
 ReactDOM.render(
   <AzureAD provider={AuthProviderFactory} forceLogin={true}>
-    {({ login, logout, authenticationState, accountInfo }: any) => {
-      console.log(accountInfo);
-      return (
-        <BrowserRouter>
-          <App
-            logout={logout}
-            accountIdentifer={accountInfo.account.accountIdentifer}
-          />
-        </BrowserRouter>
-      );
+    {({
+      login,
+      logout,
+      authenticationState,
+      accountInfo
+    }: IAzureADFunctionProps) => {
+      if (authenticationState === AuthenticationState.Authenticated) {
+        return (
+          <BrowserRouter>
+            <App
+              logout={logout}
+              accountIdentifer={accountInfo.account.accountIdentifier}
+            />
+          </BrowserRouter>
+        );
+      } else {
+        return (
+          <div
+            style={{
+              display: 'flex',
+              flexDirection: 'row',
+              justifyContent: 'center',
+              alignItems: 'center',
+              height: '100vh'
+            }}
+          >
+            <Spinner label="Waiting for login..." size={SpinnerSize.large} />
+          </div>
+        );
+      }
     }}
   </AzureAD>,
   document.getElementById('root')
--- expensely-frontend/src/utils/AuthProviderFactory.ts
+++ expensely-frontend/src/utils/AuthProviderFactory.ts
@@ -1,11 +1,10 @@
-import { Configuration, AuthenticationParameters, MsalAuthProvider } from 'react-aad-msal';
+import { Configuration, AuthenticationParameters, MsalAuthProvider, LoginType } from 'react-aad-msal';
 
 const config: Configuration = {
   auth: {
     authority:
-      'https://login.microsoftonline.com/tfp/expensely.onmicrosoft.com//B2C_1_expensely_signup',
+      'https://login.microsoftonline.com/tfp/expensely.onmicrosoft.com/B2C_1_expensely_signup/',
     clientId: 'ac6588f2-aba7-4638-b015-89ac51cd01d0',
-    // redirectUri: 'http://localhost:3000',
     redirectUri: window.location.origin
   },
   cache: {
@@ -18,4 +17,4 @@ const authenticationParameters: AuthenticationParameters = {
   scopes: ['https://expensely.onmicrosoft.com/api/user_impersonation']
 };
 
-export const AuthProviderFactory = new MsalAuthProvider(config, authenticationParameters);
+export const AuthProviderFactory = new MsalAuthProvider(config, authenticationParameters, LoginType.Popup);
